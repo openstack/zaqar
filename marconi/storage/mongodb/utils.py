1810ffa8c93f2fd1ce19a95475fdce760f9ae0d8
Flaper Fesp <flaper87@gmail.com>
2013-03-21 10:51:17 +0100

    MessageController implementation for mongodb storage
    This patch implements MessageController for the mongodb storage. In
    order to test the changes made here, it was necessary to modify current
    base classes.
    
    This patch also adds more granular exceptions (QueueDoesNotExist and
    MessageDoesNotExist) both based on the existing exception DoesNotExist.
    
    Implements blueprint storage-base
    Implements blueprint storage-mongodb
    
    Change-Id: I0752c5d797104a9cc58efd8e866b3b627646bce5

---
+++ marconi/storage/mongodb/utils.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2013 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import calendar
+
+from bson import errors as berrors
+from bson import objectid
+
+from marconi.openstack.common import timeutils
+
+
+def to_oid(obj):
+    """
+    Creates a new ObjectId based on the input
+    and raises ValueError whenever a TypeError
+    or InvalidId error is raised by the ObjectID
+    class.
+
+    :param obj: Anything that can be passed as an
+        input to `objectid.ObjectId`
+    """
+    try:
+        return objectid.ObjectId(obj)
+    except (TypeError, berrors.InvalidId):
+        msg = _("Wrong id %s") % obj
+        raise ValueError(msg)
+
+
+def oid_ts(oid):
+    """
+    Creates a non-tz-aware timestamp based on
+    the incoming object id datetime information.
+    """
+    try:
+        norm_time = timeutils.normalize_time(oid.generation_time)
+        return calendar.timegm(norm_time.timetuple())
+    except AttributeError:
+        raise TypeError(_("Expected ObjectId and got %s") % type(oid))
