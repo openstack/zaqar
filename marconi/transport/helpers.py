62572bd61da453e72e4889d91e3a3e8f4e5ed2d6
Zhihao Yuan <lichray@gmail.com>
2013-04-19 13:01:22 -0400

    Add simplejson into dependency.
    Actually, keystone-client enforces simplejson.  We can do that too.
    
    Change-Id: If8c082e55269766d7a0653f87d640f79bc8ad336

--- marconi/transport/helpers.py
+++ marconi/transport/helpers.py
@@ -13,10 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-try:
-    import simplejson as json
-except ImportError:
-    import json
+import simplejson as json
 
 
 class MalformedJSON(Exception):
