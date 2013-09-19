9371a7539bbef5b36782d92f03391a02efffc90e
kgriffs <kurt.griffiths@rackspace.com>
2013-09-19 15:14:54 -0500

    chore: Expose commit hash in version module
    This patch adds a marconi.version.verify_sha() convenience function,
    so that continuous integration scripts can use it to verify marconi has
    been installed correctly and/or to detect when the installed version is
    out of date (i.e., that "import marconi" pulls in the latest version of
    the library.)
    
    Change-Id: I645c3a55a837d588fec55fa9b5c1ec579026b8f5

--- marconi/version.py
+++ marconi/version.py
@@ -13,6 +13,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import pbr.packaging
 import pbr.version
 
 version_info = pbr.version.VersionInfo('marconi')
+
+
+def verify_sha(expected):
+    """Verifies the commit hash for an interim Marconi build.
+
+    This function may be used to verify that the version of the marconi
+    package, as imported from an environment's site-packages, is the
+    expected build. This allows continuous integration scripts to
+    detect out-of-date installations of the package.
+
+    Note that this function will ALWAYS return False for Marconi packages
+    that were not installed from git.
+
+    :param expected: The expected commit object name. May be either a full
+        or abbreviated SHA hash. If abbreviated, at least 7 digits are
+        required.
+    :returns: True if the package's version string contains a hash, and
+        that hash matches `expected`. Otherwise returns False.
+    """
+
+    # NOTE(kgriffs): Require 7 digits to avoid false positives. In practice,
+    # Git's abbreviated commit oject names will always include at least
+    # 7 digits.
+    assert len(expected) >= 7
+
+    # NOTE(kgriffs): Git usually abbreviates hashed to 7 digits, but also
+    # check 8 digits in case git decides just 7 is ambiguous. Accordingly,
+    # try the longer one first since it is more specific than the other.
+    for abbreviated in (expected[:8], expected[:7]):
+        if ('.g' + abbreviated) in version_info.release_string():
+            return True
+
+    return False
