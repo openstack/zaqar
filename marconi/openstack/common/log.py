4b39ab06edccc9353ffa93596073ecbb0944b3be
kgriffs <kurt.griffiths@rackspace.com>
2013-09-25 22:18:40 -0400

    Update oslo to latest and greatest
    This was done mainly to pull in the fixt to timeutils.set_time_override() so
    we don't have to keep passing in utcnow() into it.
    
    Change-Id: I9918d03c57b8fbdc3750c7cb072a1aff2b2c653d

--- marconi/openstack/common/log.py
+++ marconi/openstack/common/log.py
@@ -260,14 +260,14 @@ class ContextAdapter(BaseLoggerAdapter):
             extra.update(_dictify_context(context))
 
         instance = kwargs.pop('instance', None)
+        instance_uuid = (extra.get('instance_uuid', None) or
+                         kwargs.pop('instance_uuid', None))
         instance_extra = ''
         if instance:
             instance_extra = CONF.instance_format % instance
-        else:
-            instance_uuid = kwargs.pop('instance_uuid', None)
-            if instance_uuid:
-                instance_extra = (CONF.instance_uuid_format
-                                  % {'uuid': instance_uuid})
+        elif instance_uuid:
+            instance_extra = (CONF.instance_uuid_format
+                              % {'uuid': instance_uuid})
         extra.update({'instance': instance_extra})
 
         extra.update({"project": self.project})
