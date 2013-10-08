558ac2575423662c5787fa7c8285b9d3a237e6ec
Alejandro Cabrera <alejandro.cabrera@rackspace.com>
2013-10-08 15:31:06 -0400

    feat: storage sharding schema for marconi-queues
    This provides a base jsonschema for handling PATCH and PUT requests to
    a future /v1/shards/{shard_name} endpoint.
    
    Change-Id: I10c817ff8b5c196a2a3b2f5562e1bafc34e30adf
    Partially-Implements: blueprint storage-sharding

---
+++ marconi/common/schemas/shards.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2013 Rackspace Hosting, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""shards: JSON schema for marconi-queues shards resources."""
+
+# NOTE(cpp-cabrera): options can be anything. These will be unique to
+# each storage driver, so we don't perform any further validation at
+# the transport layer.
+patch_options = {
+    'type': 'object', 'properties': {
+        'options': {
+            'type': 'object'
+        }
+    }
+}
+
+# NOTE(cpp-cabrera): a string valid for use in a URI
+# TODO(cpp-cabrera): perhaps validate this further using jsonschema's
+# uri validator as per rfc3987
+patch_location = {
+    'type': 'object', 'properties': {
+        'location': {
+            'type': 'string'
+        },
+        'additionalProperties': False
+    }
+}
+
+patch_weight = {
+    'type': 'object', 'properties': {
+        'weight': {
+            'type': 'integer', 'minimum': 0, 'maximum': 2**32 - 1
+        },
+        'additionalProperties': False
+    }
+}
+
+create = {
+    'type': 'object', 'properties': {
+        'weight': patch_weight['properties']['weight'],
+        'location': patch_location['properties']['location'],
+        'options': patch_options['properties']['options']
+    },
+    # NOTE(cpp-cabrera): options need not be present. Storage drivers
+    # must provide reasonable defaults.
+    'required': ['location', 'weight'],
+    'additionalProperties': False
+}
