b20519cb174d84a0db64171f4c96b542289c04da
Flavio Percoco <flaper87@gmail.com>
2013-11-22 16:05:51 +0100

    Remove proxy code
    As discussed in one our meetings, the proxy code has been deprecated in
    favor of the new sharding work. This patch removes all the code related
    to the proxy.
    
    Partially-implements bp storage-sharding
    
    Change-Id: I6622332707c5a53ab56fa2796bef73be9e8c8c49

--- marconi/proxy/storage/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Marconi proxy storage drivers"""
-
-from marconi.proxy.storage import base
-from marconi.proxy.storage import errors  # NOQA
-
-# NOTE(cpp-cabrera): Hoist classes into package namespace
-CatalogueBase = base.CatalogueBase
-DriverBase = base.DriverBase
-PartitionsBase = base.PartitionsBase
--- marconi/proxy/transport/wsgi/__init__.py
+++
@@ -1,6 +0,0 @@
-"""WSGI Proxy Transport Driver"""
-
-from marconi.proxy.transport.wsgi import driver
-
-# Hoist into package namespace
-Driver = driver.DriverBase
