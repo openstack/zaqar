lichray/marconi

chore: Pass cache into drivers
This patch plumbs storage drivers so they all get a cache instance
they can use at their discretion.

I decided to make cache a required param to the initializers, since
any deployment of Marconi of any significance would likely opt to
enable caching anyway, and the cache library has a default in-memory
backend that can be used for simple deployments.

Note also that both data- and control-plane drivers receive a cache
instance, even though the sharding controllers will not use it
(caching will be done in the sharding data driver instead.) I thought
it would be better to pass cache in both cases so we can share test
code and avoid complicating utils.load_storage_driver(). Also, the
control driver may eventually support operations other than sharding;
cache may come in handy then.

Change-Id: I647791af0d7a5914c30cb2489033ec650a455370
Signed-off-by: kgriffs <kurt.griffiths@rackspace.com>

tests/unit/queues/storage/test_impl_mongodb.py
@@ -20,6 +20,7 @@ from pymongo import cursor
 import pymongo.errors
 from testtools import matchers
 
+from marconi.common.cache import cache as oslo_cache
 from marconi.openstack.common import timeutils
 from marconi.queues import storage
 from marconi.queues.storage import errors
@@ -81,7 +82,8 @@ class MongodbDriverTest(testing.TestBase):
         self._conf = self.load_conf('wsgi_mongodb.conf')
 
     def test_db_instance(self):
-        driver = mongodb.DataDriver(self._conf)
+        cache = oslo_cache.get_cache(self._conf)
+        driver = mongodb.DataDriver(self._conf, cache)
 
         databases = driver.message_databases + [driver.queues_database]
         for db in databases:

openstack/zaqar

tests/unit/queues/storage/test_impl_mongodb.py
@@ -21,6 +21,7 @@ from pymongo import cursor
 import pymongo.errors
 from testtools import matchers
 
+from marconi.common.cache import cache as oslo_cache
 from marconi.openstack.common import timeutils
 from marconi.queues import storage
 from marconi.queues.storage import errors
@@ -92,7 +93,8 @@ class MongodbDriverTest(testing.TestBase, MongodbTestMixin):
     config_file = 'wsgi_mongodb.conf'
 
     def test_db_instance(self):
-        driver = mongodb.DataDriver(self.conf)
+        cache = oslo_cache.get_cache(self.conf)
+        driver = mongodb.DataDriver(self.conf, cache)
 
         databases = (driver.message_databases +
                      [driver.queues_database])