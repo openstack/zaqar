52a8ef1667e53dc5abf85169db9111cb2621ae28
Malini Kamalambal <malini.kamalambal@rackspace.com>
2013-12-09 10:27:44 -0500

    Verify response jsonschema for List Messages by ID
    This patch verifies that the API response for List Messages by ID
    matches the expected jsonschema.
    
    Change-Id: Icd6fcab5d36ce79b8030017eff73815832fedf59
    Tests-Bug: #1240897

---
+++ marconi/queues/api/v1/response.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2013 Rackspace, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from marconi.common import api
+
+
+class ResponseSchema(api.Api):
+
+    """Define validation schema for json response."""
+
+    def __init__(self, limits):
+        self.limits = limits
+        self.schema = {
+            'message_get_many':
+            {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "properties": {
+                        "href": {
+                            "type": "string",
+                            "pattern": "^(/v1/queues/[a-zA-Z0-9_-]{1,64}"
+                            "/messages/[a-zA-Z0-9_-]+)$"
+                        },
+                        "ttl": {
+                            "type": "number",
+                            "minimum": 1,
+                            "maximum": self.limits.message_ttl_max
+                        },
+                        "age": {
+                            "type": "number",
+                            "minimum": 0
+                        },
+                        "body": {
+                            "type": "object"
+                        }
+                    },
+                    "required": ["href", "ttl", "age", "body"]
+                },
+                "minItems": 1,
+                "maxItems": self.limits.message_paging_uplimit
+            }
+        }
