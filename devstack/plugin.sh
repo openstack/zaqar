172dd3824a90003ed9be9f065601c73d88453cf1
Flavio Percoco <flaper87@gmail.com>
2015-05-05 23:24:25 +0200

    Add function dispatchers
    Add devstack function dispatchers so that these functions will be
    actually called when stack.sh is executed.
    
    Change-Id: Ia9a6ecef8dfff93e39c507bbbb25fbc544dfaf48

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -231,6 +231,36 @@ function create_zaqar_accounts {
 }
 
 
+
+
+if is_service_enabled zaqar-server; then
+    if [[ "$1" == "stack" && "$2" == "install" ]]; then
+        echo_summary "Installing Zaqar"
+        install_zaqarclient
+        install_zaqar
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        echo_summary "Configuring Zaqar"
+        configure_zaqar
+        configure_zaqarclient
+
+        if is_service_enabled key; then
+           create_zaqar_accounts
+        fi
+
+    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+        echo_summary "Initializing Zaqar"
+        init_zaqar
+        start_zaqar
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        stop_zaqar
+    fi
+
+    #if [[ "$1" == "clean" ]]; then
+    #fi
+fi
+
 # Restore xtrace
 $XTRACE
 
