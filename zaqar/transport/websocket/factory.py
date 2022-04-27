2abee24a02a439de39ddf88a5688093fc5969e14
wanghao <sxmatch1986@gmail.com>
2022-04-27 13:46:31 +0800

    Add python3.8 requirement
    According to the QA's request[1], we should drop the python3.6
    support in Zed cycle, and add the requirement to python3.8.
    
    [1]:http://lists.openstack.org/pipermail/openstack-discuss/2022-April/028321.html
    
    Change-Id: I772621f28d723c88e227edd3b540ce133ea3feb2

--- zaqar/transport/websocket/factory.py
+++ zaqar/transport/websocket/factory.py
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import txaio
+txaio.use_asyncio()
+
 from autobahn.asyncio import websocket
 import msgpack
 from oslo_serialization import jsonutils
