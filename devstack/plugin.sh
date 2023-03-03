c611c5c6e8d3e999d378c276094e353d01cd22c2
Erik Olof Gunnar Andersson <eandersson@blizzard.com>
2023-03-03 15:51:37 -0800

    Unblock CI by fixing libssl url
    The manually provided libssl package no longer exists.
    
    Change-Id: I5735d203076175cb6acc32ec32145a6cf255a63d

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -193,8 +193,8 @@ function configure_mongodb {
             wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
             echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
             sudo apt update
-            curl -LO http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.20_amd64.deb
-            sudo dpkg -i ./libssl1.1_1.1.1-1ubuntu2.1~18.04.20_amd64.deb
+            curl -LO http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.21_amd64.deb
+            sudo dpkg -i ./libssl1.1_1.1.1-1ubuntu2.1~18.04.21_amd64.deb
             install_package mongodb-org
             restart_service mongod
         else
--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -38,8 +38,8 @@ if [[ $ubuntu_version > '22' ]]; then
     wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
     echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
     sudo apt update
-    curl -LO http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.20_amd64.deb
-    sudo dpkg -i ./libssl1.1_1.1.1-1ubuntu2.1~18.04.20_amd64.deb
+    curl -LO http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.21_amd64.deb
+    sudo dpkg -i ./libssl1.1_1.1.1-1ubuntu2.1~18.04.21_amd64.deb
     sudo apt install -y mongodb-org
     sudo systemctl restart mongod
 else
