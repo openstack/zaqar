54aed6be0f2b5a53042185b481587e445e923c8b
Martin André <m.andre@redhat.com>
2023-03-07 19:22:10 +0100

    Fix install of mongodb on Ubuntu 22.04
    Simplify the version check to make it less error prone, use the right
    version of the 22.04 repository from mongodb.org (jammy vs focal) and
    stop trying to install libssl package that no longer exists on the
    repository.
    
    Change-Id: I49d32e555e6a0df15c0e059e8c4152a969ab3975

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -176,10 +176,10 @@ function configure_mongodb {
     pip_install pymongo
     if is_ubuntu; then
         # NOTE: To fix the mongodb's issue in ubuntu 22.04 LTS
-        ubuntu_version=`cat /etc/issue | cut -d " "  -f2`
-        if [[ $ubuntu_version > '22' ]]; then
+        ubuntu_version=$(source /etc/os-release ; echo $VERSION_ID)
+        if [[ $ubuntu_version == '22.04' ]]; then
             wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
-            echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+            echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
             sudo apt update
             curl -LO http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.21_amd64.deb
             sudo dpkg -i ./libssl1.1_1.1.1-1ubuntu2.1~18.04.21_amd64.deb
