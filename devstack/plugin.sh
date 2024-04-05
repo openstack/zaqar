92101df6279bbb1a35e55fbdb1449b76045a87e2
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-04-05 10:49:14 +0900

    Do not enable bionic-security repository
    We no longer use Bionic in recent versions.
    
    Change-Id: Ie736769d28699f9d1be8146d4e2f66c7f4742bdd

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -191,10 +191,7 @@ function configure_mongodb {
         if [[ $ubuntu_version == '22.04' ]]; then
             wget -qO - https://www.mongodb.org/static/pgp/server-7.0.asc | sudo apt-key add -
             echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-            echo "deb http://security.ubuntu.com/ubuntu bionic-security main" | sudo tee /etc/apt/sources.list.d/bionic-security.list
-            sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
             sudo apt update
-            install_package libssl1.1
             install_package mongodb-org
             restart_service mongod
             sudo systemctl status mongod
