6123c9d7ab459915ab4a9d764d1123b5c13e7da3
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-04-05 10:49:14 +0900

    Do not enable bionic-security repository
    We no longer use Bionic in recent versions.
    
    Change-Id: Ie736769d28699f9d1be8146d4e2f66c7f4742bdd
    (cherry picked from commit 92101df6279bbb1a35e55fbdb1449b76045a87e2)

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -37,20 +37,14 @@ ubuntu_version=$(source /etc/os-release ; echo $VERSION_ID)
 if [[ $ubuntu_version == '22.04' ]]; then
     wget -qO - https://www.mongodb.org/static/pgp/server-7.0.asc | sudo apt-key add -
     echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-    echo "deb http://security.ubuntu.com/ubuntu bionic-security main" | sudo tee /etc/apt/sources.list.d/bionic-security.list
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
     sudo apt update
-    sudo apt install -y libssl1.1
     sudo apt install -y mongodb-org
     sudo systemctl restart mongod
     sudo systemctl status mongod
 elif [[ $ubuntu_version == '12' ]]; then
     wget -qO - https://www.mongodb.org/static/pgp/server-7.0.asc | sudo apt-key add -
     echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
-    echo "deb http://security.ubuntu.com/ubuntu bionic-security main" | sudo tee /etc/apt/sources.list.d/bionic-security.list
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
     sudo apt update
-    sudo apt install -y libssl1.1
     sudo apt install -y mongodb-org
     sudo systemctl restart mongod
     sudo systemctl status mongod
