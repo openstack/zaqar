59a7a63d83077ada01ebadc2db140d8109e06146
Takashi Kajinami <kajinamit@oss.nttdata.com>
2026-01-21 21:09:25 +0900

    Fix mongodb installation in Debian Trixie
    Debian Trixie is now used to run py313 job.
    
    Change-Id: Ie25ed1ce681d357ae134c0f1cc0eacdca8c77886
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -52,8 +52,21 @@ elif [[ $ubuntu_version == '22.04' ]]; then
     sudo systemctl restart mongod
     sudo systemctl status mongod
 elif [[ $ubuntu_version == '12' ]]; then
-    wget -qO - https://www.mongodb.org/static/pgp/server-8.0.asc | sudo apt-key add -
-    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/debian bookworm/mongodb-org/8.0 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+    curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+        sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
+        --dearmor
+    echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/debian bookworm/mongodb-org/8.0 main" | \
+        sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+    sudo apt update
+    sudo apt install -y mongodb-org
+    sudo systemctl restart mongod
+    sudo systemctl status mongod
+elif [[ $ubuntu_version == '13' ]]; then
+    curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+        sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
+        --dearmor
+    echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/debian bookworm/mongodb-org/8.0 main" | \
+        sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
     sudo apt update
     sudo apt install -y mongodb-org
     sudo systemctl restart mongod
